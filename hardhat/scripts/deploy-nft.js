05f4c276d8242b6ea73b3176d76623cfdc643cf4
Shakib KATAYA <kataya.shakib@gmail.com>
2023-06-25 11:52:34 +0300

    Create deploy-nft.js & Redeploy CryptoDevs.sol

---
+++ hardhat/scripts/deploy-nft.js
@@ -0,0 +1,36 @@
+const hre = require("hardhat");
+
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function main() {
+  const whitelistAddress = "0x383ed2396239dCe1C9c25068Df93a5B91Ea93356";
+  // Deploy the CryptoDevs Contract
+  const nftContract = await hre.ethers.deployContract("CryptoDevs", [
+    whitelistAddress,
+  ]);
+
+  // wait for the contract to deploy
+  await nftContract.waitForDeployment();
+
+  // print the address of the deployed contract
+  console.log("NFT Contract Address:", nftContract.target);
+
+  // Sleep for 30 seconds while Etherscan indexes the new contract deployment
+  await sleep(30 * 1000); // 30s = 30 * 1000 milliseconds
+
+  // Verify the contract on etherscan
+  await hre.run("verify:verify", {
+    address: nftContract.target,
+    constructorArguments: ["0x383ed2396239dCe1C9c25068Df93a5B91Ea93356"],
+  });
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
