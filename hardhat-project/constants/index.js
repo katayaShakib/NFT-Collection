1f62980eae9c160056f01132a1bc1c939818c346
Shakib KATAYA <kataya.shakib@gmail.com>
2023-06-25 11:53:09 +0300

    Delete next-app & old hh-project

--- hardhat-project/constants/index.js
+++
@@ -1,6 +0,0 @@
-// Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0xd615B39C35Ac7128E7554E62A0AA3b7Ac99e04E0";
-// URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-livid-eta.vercel.app/api/";
-
-module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
